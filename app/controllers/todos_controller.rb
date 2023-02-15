99f8b9aac436f7306c2d28ec29bbdf6a03679d4b
Won Rhim <wwrk22@gmail.com>
2023-02-14 23:40:24 -0500

    Allow user to create a new todo item and view it on the root page

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,26 @@
 class TodosController < ApplicationController
   def index
+    @todos = Todo.all
+  end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.new(new_todo_params)
+
+    if @todo.save
+      redirect_to todos_path, notice: "Added new todo"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+
+  def new_todo_params
+    params.require(:todo).permit(:title)
   end
 end
