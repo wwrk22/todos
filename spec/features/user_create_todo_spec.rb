5e550a16070a87adb95072e9719228ad3c633265
Won Rhim <wwrk22@gmail.com>
2023-02-15 13:11:12 -0500

    Use Input tag id attribute over the Label tag content

--- spec/features/user_create_todo_spec.rb
+++ spec/features/user_create_todo_spec.rb
@@ -5,7 +5,7 @@ feature "User creates todo" do
     visit root_path
 
     click_on "Add a new todo"
-    fill_in "Title", with: "Buy milk"
+    fill_in "todo_title", with: "Buy milk"
     click_on "Create Todo"
 
     expect(page).to have_css '.todos li', text: "Buy milk"
